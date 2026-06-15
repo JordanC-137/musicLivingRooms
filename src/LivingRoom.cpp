8bb85e4c3c112b08ba31cef8904bb84108a3f966
Jordan <jordanasiama3@gmail.com>
2026-06-15 21:30:33 +0100

    Adding collection to library

--- src/LivingRoom.cpp
+++ src/LivingRoom.cpp
@@ -4,3 +4,10 @@ LivingRoom::LivingRoom(int id)
     : m_id{ id }
 {
 }
+
+int LivingRoom::add_album(int id, std::string title, std::string artist)
+{
+    Album album{id, title, artist};
+    m_collection.push_back(album);
+    return 0;
+}
