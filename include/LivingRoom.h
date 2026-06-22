04fdb020a572899bf7d47b645a84b7161ed50f31
Jordan <jordanasiama3@gmail.com>
2026-06-22 10:14:39 +0100

    Initial display

--- include/LivingRoom.h
+++ include/LivingRoom.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "Album.h"
+#include <string_view>
 
 class LivingRoom{
     int m_id;
@@ -12,6 +13,9 @@ public:
     LivingRoom(int id);
 
     int add_album(int id, std::string title, std::string artist);
+
+    std::vector<std::string_view> basic_display();
+
 };
 
 #endif
--- src/LivingRoom.cpp
+++ src/LivingRoom.cpp
@@ -11,3 +11,18 @@ int LivingRoom::add_album(int id, std::string title, std::string artist)
     m_collection.push_back(album);
     return 0;
 }
+
+std::vector<std::string_view> LivingRoom::basic_display()
+{
+    int count {0};
+    std::vector<std::string_view> result{};
+
+    std::vector<Album>::iterator it{m_collection.begin()};
+    while(count < 4 & it != m_collection.end()){
+        auto title = it->getTitle();
+        result.push_back(title);
+        ++it;
+        count++;
+    }
+    return result;
+}
