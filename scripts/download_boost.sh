1fa3c5dae39648e452719c91e73a2a5271cad1fe
Jordan <jordanasiama3@gmail.com>
2026-08-18 07:34:58 +0100

    Minor mistake

--- scripts/download_boost.sh
+++ scripts/download_boost.sh
@@ -2,8 +2,7 @@
 
 wget https://archives.boost.io/release/1.92.0/source/boost_1_92_0.tar.bz2
 tar -xf boost_1_92_0.tar.bz2
-cd boost_1_92_0/
 
-./boostrap.sh --prefix=/usr/local
-./b2
-./b2 install
+./boost_1_92_0/bootstrap.sh --prefix=/usr/local
+./boost_1_92_0/b2
+./boost_1_92_0/b2 install
