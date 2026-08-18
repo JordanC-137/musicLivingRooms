a27eae686100eb8d09097583a7930be5259202b6
Jordan <jordanasiama3@gmail.com>
2026-08-18 20:19:34 +0100

    Bit more info

--- scripts/download_boost.sh
+++ scripts/download_boost.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-wget https://archives.boost.io/release/1.92.0/source/boost_1_92_0.tar.bz2
+wget --quiet https://archives.boost.io/release/1.92.0/source/boost_1_92_0.tar.bz2
 tar -xf boost_1_92_0.tar.bz2
 
 sudo ./boost_1_92_0/bootstrap.sh --prefix=/usr/local
+echo "Run b2"
 sudo ./b2
+
+echo "Run b2 headers"
 sudo ./b2 headers
+
+echo "Run b2 install"
 sudo ./b2 install --prefix=/usr/local
