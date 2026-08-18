47912b2f9e525684609848ed3a1eaccc0d88689f
Jordan <jordanasiama3@gmail.com>
2026-08-18 18:16:26 +0100

    Present dir and subdirs

--- scripts/download_boost.sh
+++ scripts/download_boost.sh
@@ -3,6 +3,9 @@
 wget https://archives.boost.io/release/1.92.0/source/boost_1_92_0.tar.bz2
 tar -xf boost_1_92_0.tar.bz2
 
+echo "Present dir: ${PWD}"
+echo "List Dirs"
+ls -R .
 ./boost_1_92_0/bootstrap.sh --prefix=/usr/local
 ./boost_1_92_0/b2
 ./boost_1_92_0/b2 install
