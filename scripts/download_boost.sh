417c94baa869f3827b3cb660695842d0a4935d96
Jordan <jordanasiama3@gmail.com>
2026-08-18 03:19:31 +0100

    Another small change

--- scripts/download_boost.sh
+++ scripts/download_boost.sh
@@ -2,7 +2,7 @@
 
 wget https://archives.boost.io/release/1.92.0/source/boost_1_92_0.tar.bz2
 tar -xf boost_1_92_0.tar.bz2
-cd boost_1_92_0.tar.bz2
+cd boost_1_92_0.tar/
 
 ./boostrap.sh --prefix=/usr/local
 ./b2
