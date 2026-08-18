a4532a4763e32eb7c39cc98f9ce7915205cf2049
Jordan <jordanasiama3@gmail.com>
2026-08-18 18:43:17 +0100

    Remove clutter

--- scripts/download_boost.sh
+++ scripts/download_boost.sh
@@ -4,8 +4,5 @@ wget https://archives.boost.io/release/1.92.0/source/boost_1_92_0.tar.bz2
 tar -xf boost_1_92_0.tar.bz2
 
 sudo ./boost_1_92_0/bootstrap.sh --prefix=/usr/local
-
-echo "Present dir: ${PWD}"
-echo "List Dirs"
-ls -R .
+sudo ./b2
 sudo ./b2 install
