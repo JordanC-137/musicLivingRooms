7a2a7782c63054a22183b146c4159bf4dbe3a33a
Jordan <jordanasiama3@gmail.com>
2026-08-18 03:15:51 +0100

    - included in tar command

--- scripts/download_boost.sh
+++ scripts/download_boost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://archives.boost.io/release/1.92.0/source/boost_1_92_0.tar.bz2
-tar xf boost_1_92_0.tar.bz2
+tar -xf boost_1_92_0.tar.bz2
 cd boost_1_92_0.tar.bz2
 
 ./boostrap.sh --prefix=/usr/local
