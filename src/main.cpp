cd6576e3272b8f4673943a8c95f70fbd9c0b47ee
Jordan <66384172+JordanC-137@users.noreply.github.com>
2026-09-03 00:58:20 +0100

    Initial movement towards networking (#6)
    * Basic initial GET function
    
    * Attempt at installing boost
    
    * Adjustment
    
    * A1
    
    * A2
    
    * use GITHUB_WORKSPACE
    
    * spelling mistake
    
    * Added sudo
    
    * - included in tar command
    
    * Another small change
    
    * Further
    
    * Minor mistake
    
    * Present dir and subdirs
    
    * boostrap line up
    
    * b2 move
    
    * Remove clutter
    
    * Still changing
    
    * Bit more info
    
    * Boop
    
    * Download boost via apt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,58 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
 #include <iostream>
+#include <string_view>
 #include "LivingRoom.h"
 
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+// Initialise connection
+int get_home(){
+	try{
+		auto const host = "localhost";
+		auto const port = "8080";
+		auto const target = "/albums";
+		int version = 11;
+
+		net::io_context ioc;
+		tcp::resolver resolver(ioc);
+		beast::tcp_stream stream(ioc);
+
+		//Look up domain name
+		auto const results = resolver.resolve(host, port);
+		stream.connect(results);
+
+		http::request<http::string_body> req {http::verb::get, target, version};
+		req.set(http::field::host, host);
+		req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+		http::write(stream, req);
+		beast::flat_buffer buffer;
+		http::response<http::dynamic_body> res;
+		http::read(stream, buffer, res);
+
+		std::cout << res << std::endl;
+
+		//Close socket
+		beast::error_code ec;
+		stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+	}
+	catch(std::exception const& e){
+		std::cerr << "Error" << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
 int main(){
+	get_home();
 	LivingRoom lr {1};
 	lr.add_album(0, "Hunky Dory", "David Bowie");
 	lr.add_album(1, "Ziggy", "David Bowie");
