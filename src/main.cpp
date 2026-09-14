3ccafc35e3d89b8dc5dcbe6127a916ae1c234307
Jordan <jordanasiama3@gmail.com>
2026-09-14 01:19:44 +0100

    demo post request

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 
 #include <cpr/cpr.h>
 
+long postRequest(std::string_view sv){
+	cpr::Response r = cpr::Post(cpr::Url{sv},
+						cpr::Header{{"Content-Type", "application/json"}},
+						cpr::Body{"{\"title\":\"Ziggy Stardust\"}"});
+	std::cout << r.text << std::endl;
+	return r.status_code;
+}
+
 long getRequest(std::string_view sv){
 	cpr::Response r = cpr::Get(cpr::Url(sv),
 	cpr::Header{{"accept", "application/json"}});
@@ -11,7 +19,7 @@ long getRequest(std::string_view sv){
 }
 
 int main(){
-	int sc {getRequest("http://www.httpbin.org")};
+	int sc {postRequest("localhost:8080/albums")};
 	std::cout << "SC: " << sc << std::endl;
 	return 0;
 }
